f489016e46eb093bfc7fb31f45878aeb953839cb
Swathi-pandre <pandreswathi45@gmail.com>
2025-04-02 06:42:10 +0530

    core java

--- Day2/Main.java
+++ Day2/Main.java
@@ -1,52 +1,13 @@
-package Day2and3;
+package library;  // ✅ All files are in the same package
 
-class Vehicle {
-    int speed;
-    String fuelType;
+import static library.Utility.generateId; // ✅ Static import for generateId()
 
-    Vehicle(int speed, String fuelType) {
-        this.speed = speed;
-        this.fuelType = fuelType;
-    }
-
-    void displayDetails() {
-        System.out.println("Speed: " + speed + " km/h, Fuel Type: " + fuelType);
-    }
-}
-
-class Car extends Vehicle {
-    int numDoors;
-
-    Car(int speed, String fuelType, int numDoors) {
-        super(speed, fuelType);
-        this.numDoors = numDoors;
-    }
-
-    @Override
-    void displayDetails() {
-        super.displayDetails();
-        System.out.println("Number of Doors: " + numDoors);
-    }
-}
-
-class ElectricCar extends Car {
-    int batteryCapacity;
-
-    ElectricCar(int speed, String fuelType, int numDoors, int batteryCapacity) {
-        super(speed, fuelType, numDoors);
-        this.batteryCapacity = batteryCapacity;
-    }
-
-    @Override
-    void displayDetails() {
-        super.displayDetails();
-        System.out.println("Battery Capacity: " + batteryCapacity + " kWh");
-    }
-}
-
-class Main {
+public class Main {
     public static void main(String[] args) {
-        ElectricCar ec = new ElectricCar(60, "Diesel", 4, 5000);
-        ec.displayDetails();
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald");
+        Members member1 = new Members("Alice", generateId());  // ✅ Make sure your class is "Members" and not "Member"
+
+        book1.displayBookInfo();  
+        member1.displayMemberInfo();
     }
 }
